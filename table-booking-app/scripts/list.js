1c0ffeec4e2caa17e68fc644cb682f507f16fc95
a-veloo <a.veloo@gmail.com>
2020-10-29 14:29:19 +0800

    added table.deleteRow

--- table-booking-app/scripts/list.js
+++ table-booking-app/scripts/list.js
@@ -51,7 +51,10 @@ function DeleteBooking(id){
           method: 'DELETE',
         })
         .then((response) => {
-          //error here prevented a refresh
+          let table = document.getElementById("booking-list");
+          for (let i =1; i < table.rows.length; i++){
+              table.deleteRow(i);
+          }
           GetBookings();
         });
     }else{
