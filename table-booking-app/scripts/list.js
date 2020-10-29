5b212adef5a0eb09d67021d45d9e8627054eeb39
a-veloo <a.veloo@gmail.com>
2020-10-29 08:53:07 +0800

    added list js code

---
+++ table-booking-app/scripts/list.js
@@ -0,0 +1,45 @@
+document.addEventListener("load",function(){
+    GetBookings();
+});
+
+function GetBookings(){
+    //Wwhile let can be only available inside the scope it's declared, like in for loop, var can be //accessed outside the loop for example.
+      let url = 'https://api.sheety.co/785e504f8ad8792780687397b8ec927c/bookingApp/bookings';
+      fetch(url)
+      
+      .then((response) => response.json())
+      .then(json => {
+        // Do something with the data
+        //console.log(json.bookings);
+        var bookings = document.getElementById("booking-list");
+        var bookingIds = [];
+        
+        for(i=0; i<json.bookings.length; i++){
+          var aName = json.bookings[i].name;
+          var aEmail = json.bookings[i].email;
+          var aPax = json.bookings[i].pax;
+          var aId = json.bookings[i].id;
+          // new variable
+          var buttonId = "delete" +aId;
+
+          let row = bookings.insertRow(bookings.rows.length);
+          row.insertCell(0).innerHTML = aId;
+          row.innerHTML(1).innerHTML = aName;
+          row.innerHTML(2).innerHTML = aEmail;
+          row.innerHTML(3).innerHTML = aPax;
+          row.innerHTML(4).innerHTML =""; //Remarks
+          row.insertCell(5).innerHTML = "<button id='" +buttonId +"'>Delete</button><br />";
+          
+          bookingIds.push(buttonId);
+          //console.log(buttonId);  
+        }
+        for(let j=0; j<bookingIds.length; j++){
+          let el = document.getElementById(bookingIds[j]);
+          
+          el.addEventListener("click",function(){
+            let theId = bookingIds[j].replace("delete","");
+            //DeleteBooking(theId);
+          });
+        }
+      });
+    }
