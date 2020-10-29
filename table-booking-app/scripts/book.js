afd84267ea8536bd72feb0fba6f575c8dd6648ce
a-veloo <a.veloo@gmail.com>
2020-10-29 22:29:56 +0800

    added ids to book.html and event listener in book.js

---
+++ table-booking-app/scripts/book.js
@@ -0,0 +1,37 @@
+function BookNow(guestName,guestEmail,guestPax){
+    let url = 'https://api.sheety.co/785e504f8ad8792780687397b8ec927c/bookingApp/bookings';
+    
+    let body = {
+      //data object to send to the server
+      booking: {
+        //key:value pairs
+        name:guestName,
+        email:guestEmail,
+        pax:guestPax
+      }
+    }
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(body),
+      //header so server/api knows the content request type is json, in order to add data.
+      headers:{
+        "Content-Type":"application/json"
+      }
+    })
+    .then((response) => response.json())
+    .then(json => {
+      // Do something with object
+      console.log(json.booking);
+      document.getElementById("bookMsg").innerHTML = json.booking.name +" successfully added!";
+      GetBookings();
+    });
+}
+
+document.getElementById("bookNow").addEventListener("click", function(){
+    let name = document.getElementById("guestName");
+    let name = document.getElementById("guestEmail");
+    let name = document.getElementById("guestPax");
+    let remarks = document.getElementById("guestRemarks");
+
+    console.log(name +"," +email +"," +pax +"," +remarks);
+});
