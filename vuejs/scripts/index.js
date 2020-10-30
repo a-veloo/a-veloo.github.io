4c58bd59b4f399a2bff2baa65f93a11b8208202c
a-veloo <a.veloo@gmail.com>
2020-10-30 20:12:50 +0800

    add linkURL and LinkText

--- vuejs/scripts/index.js
+++ vuejs/scripts/index.js
@@ -10,5 +10,7 @@ var app2 = new Vue({
 el:"#app-2",
     data: {
         message:"You've loaded this page on " +new Date().toLocaleString() 
+        linkURL: "https://www.google.com.nz"
+        LinkText: "Go to Google New Zealand"
     }
 });
