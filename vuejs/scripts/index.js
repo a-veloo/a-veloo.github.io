38e2a5ccbebd6045a5c359514db17a5dc19d4040
a-veloo <a.veloo@gmail.com>
2020-10-30 20:13:25 +0800

    correct error

--- vuejs/scripts/index.js
+++ vuejs/scripts/index.js
@@ -10,7 +10,7 @@ var app2 = new Vue({
 el:"#app-2",
     data: {
         message:"You've loaded this page on " +new Date().toLocaleString() 
-        linkURL: "https://www.google.com.nz"
+        linkURL: "https://www.google.co.nz"
         LinkText: "Go to Google New Zealand"
     }
 });
