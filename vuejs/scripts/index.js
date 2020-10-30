b40709748d2166f19b7f7f904e761abfcbd45c60
a-veloo <a.veloo@gmail.com>
2020-10-30 20:00:56 +0800

    added app-2

--- vuejs/scripts/index.js
+++ vuejs/scripts/index.js
@@ -1,6 +1,14 @@
+//create a new Vue object
 var app = new Vue({ 
     el: "#app", 
     data: { 
         message: "Hello Vue!!"
+    }
+});
+
+var app2 = new Vue({
+el:"#app-2",
+    data: {
+        message:"You've loaded this page on " +new Date().toLocaleString() 
     }
 });
